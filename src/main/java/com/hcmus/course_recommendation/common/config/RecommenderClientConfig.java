f5913f0276086889ec1da8703ff56102e704ddfb
Vuong Nhat Tin <tin.vuong@navercorp.com>
2026-04-06 22:24:55 +0700

    Update to not hard-code AI URL

--- src/main/java/com/hcmus/course_recommendation/common/config/RecommenderClientConfig.java
+++ src/main/java/com/hcmus/course_recommendation/common/config/RecommenderClientConfig.java
@@ -5,17 +5,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.support.RestClientHttpServiceGroupConfigurer;
 import org.springframework.web.service.registry.ImportHttpServices;
 
+import com.hcmus.course_recommendation.auth.properties.DomainProperties;
 import com.hcmus.course_recommendation.recommendation.fs.client.FSClient;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @ImportHttpServices(types = {FSClient.class})
+@RequiredArgsConstructor
 public class RecommenderClientConfig {
+
+	private final DomainProperties domainProperties;
 
 	@Bean
 	RestClientHttpServiceGroupConfigurer groupConfigurer() {
 		return groups -> {
 			groups.forEachClient((group, builder) -> builder
-				.baseUrl("http://localhost:8000")
+				.baseUrl(domainProperties.aiUrl())
 				.build());
 		};
 	}
